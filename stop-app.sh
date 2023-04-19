5127c06fe842f1d2f539e062c94f44966ae460a0
aragorn218 <131291506+aragorn218@users.noreply.github.com>
2023-04-19 19:38:03 +0200

    Update stop-app.sh

--- stop-app.sh
+++ stop-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Zastavenie Docker kontajnera pre aplikáciu
-docker stop my-app
-
-# Zastavenie Docker kontajnera pre databázu
-docker stop webovetechnologie 
+# Delete k8s manifests in the namespace
+kubectl delete -f deployment.yaml -n my-app
+kubectl delete -f statefulset.yaml -n my-app
+kubectl delete -f service.yaml -n my-app
+kubectl delete -f namespace.yaml
