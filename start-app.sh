5714cb8b1d33992e4348d45d3a4cd12f1c09e254
aragorn218 <131291506+aragorn218@users.noreply.github.com>
2023-04-19 19:37:43 +0200

    Update start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# Zmena adresara na eknizka
+# Apply k8s manifests in the namespace
+kubectl apply -f namespace.yaml
+kubectl apply -f deployment.yaml -n my-app
+kubectl apply -f statefulset.yaml -n my-app
+kubectl apply -f service.yaml -n my-app
 
-# Spustenie Docker kontajnera pre databázu
-docker start webovetechnologie 
-
-# Spustenie Docker kontajnera pre aplikáciu
-docker start my-app
-
-# Konfigurácia kontajnerov pre znovuspustenie v prípade poruchy
-docker update --restart=always webovetechnologie 
-docker update --restart=always my-app
-
-# Získanie portu, na ktorom aplikácia beží
-APP_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "3000/tcp") 0).HostPort}}' my-app)
-
-# Vypísanie správy s portom
-echo "Aplikácia beží na porte: $APP_PORT"
